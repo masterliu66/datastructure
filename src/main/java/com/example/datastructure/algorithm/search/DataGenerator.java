4feb466cfa66d99da6b0a19c40e246c2cc9a234b
masterliu <136073894@qq.com>
2023-04-17 19:26:40 +0800

    remove unused code

--- src/main/java/com/example/datastructure/algorithm/search/DataGenerator.java
+++ src/main/java/com/example/datastructure/algorithm/search/DataGenerator.java
@@ -1,10 +1,14 @@
 package com.example.datastructure.algorithm.search;
 
+import net.datafaker.Faker;
 import net.datafaker.providers.base.Name;
 import net.datafaker.transformations.Schema;
 
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import static net.datafaker.transformations.Field.field;
 
 public class DataGenerator {
 
@@ -15,38 +19,48 @@ public class DataGenerator {
     MockDataRepository mockDataRepository = new MockDataRepository();
 
     public static void main(String[] args) {
-        new DataGenerator().generateSorted();
+        new DataGenerator().generateOrderly(new SchemaFactory().defaultSchema());
     }
 
-    public void generateSorted() {
+    public void generateOrderly(Schema<Name, String> fromScratch) {
         CsvTransformer<Name> transformer = CsvTransformer.<Name>builder().header(false).separator(",").build();
         // Schema for from scratch
-        Schema<Name, String> fromScratch = new SchemaFactory().newSchema();
         for (int i = 0; i < DATA_PARTITION_SIZE; i++) {
-            System.out.println("开始执行第" + i + "个任务");
-            String result = transformer.generate(fromScratch, DATA_SIZE);
-            System.out.println("第" + i + "个任务数据准备完成");
-            mockDataRepository.writeToPartition(result, i);
-            System.out.println("第" + i + "个任务执行完成");
+            generate(fromScratch, transformer, i);
         }
     }
 
-    public void generate() {
+    public void generateConcurrently(Schema<Name, String> fromScratch) {
         CsvTransformer<Name> transformer = CsvTransformer.<Name>builder().header(false).separator(",").build();
         // Schema for from scratch
-        Schema<Name, String> fromScratch = new SchemaFactory().newSchema();
-        ExecutorService executorService = Executors.newFixedThreadPool(16);
+        int processors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(processors << 1);
         for (int i = 0; i < DATA_PARTITION_SIZE; i++) {
             final int index = i;
-            executorService.execute(() -> {
-                System.out.println("开始执行第" + index + "个任务");
-                String result = transformer.generate(fromScratch, DATA_SIZE);
-                System.out.println("第" + index + "个任务数据准备完成");
-                mockDataRepository.writeToPartition(result, index);
-                System.out.println("第" + index + "个任务执行完成");
-            });
+            executorService.execute(() -> generate(fromScratch, transformer, index));
         }
         executorService.shutdown();
+    }
+
+    private void generate(Schema<Name, String> fromScratch, CsvTransformer<Name> transformer, int index) {
+        System.out.println("开始执行第" + index + "个任务");
+        String result = transformer.generate(fromScratch, DATA_SIZE);
+        System.out.println("第" + index + "个任务数据准备完成");
+        mockDataRepository.writeToPartition(result, index);
+        System.out.println("第" + index + "个任务执行完成");
+    }
+
+    public void testGenerate(int limit) {
+        Faker faker = new Faker(new Locale("zh", "CN"));
+        // transformer could be the same for both
+        net.datafaker.transformations.CsvTransformer<Name> transformer = net.datafaker.transformations.CsvTransformer.<Name>builder().header(true).separator(",").build();
+        // Schema for from scratch
+        Schema<Name, String> fromScratch = Schema.of(field("firstName", () -> faker.name().firstName()), field("lastname", () -> faker.name().lastName()));
+        System.out.println(transformer.generate(fromScratch, limit));
+        // Schema for transformations
+        Schema<Name, String> schemaForTransformations = Schema.of(field("firstName", Name::firstName), field("lastname", Name::lastName));
+        // Here we pass a collection of Name objects and extract first and lastnames from each element
+        System.out.println(transformer.generate(faker.collection(faker::name).maxLen(limit).generate(), schemaForTransformations));
     }
 
 }
--- src/main/java/com/example/datastructure/algorithm/search/SchemaFactory.java
+++ src/main/java/com/example/datastructure/algorithm/search/SchemaFactory.java
@@ -2,7 +2,6 @@ package com.example.datastructure.algorithm.search;
 
 import net.datafaker.Faker;
 import net.datafaker.providers.base.Name;
-import net.datafaker.transformations.CsvTransformer;
 import net.datafaker.transformations.Schema;
 
 import java.util.Locale;
@@ -16,13 +15,8 @@ public class SchemaFactory {
 
     AtomicInteger atomicInteger = new AtomicInteger();
 
-    public Schema<Name, String> newSchema() {
+    public Schema<Name, String> defaultSchema() {
         // name gender phone address birthday
-//        System.out.println(faker.name().fullName());
-//        System.out.println(faker.gender().binaryTypes());
-//        System.out.println(faker.phoneNumber().cellPhone());
-//        System.out.println(faker.date().birthday("yyyy-MM-dd hh:mm:ss"));
-//        System.out.println(faker.address().cityName());
         return Schema.of(
                 field("number", () -> String.valueOf(atomicInteger.getAndIncrement())),
                 field("name", () -> faker.name().fullName()),
@@ -31,18 +25,6 @@ public class SchemaFactory {
                 field("address", () -> faker.address().cityName()),
                 field("birthday", () -> faker.date().birthday("yyyy-MM-dd hh:mm:ss"))
         );
-    }
-
-    public void generate(int limit) {
-        // transformer could be the same for both
-        CsvTransformer<Name> transformer = CsvTransformer.<Name>builder().header(true).separator(",").build();
-        // Schema for from scratch
-        Schema<Name, String> fromScratch = Schema.of(field("firstName", () -> faker.name().firstName()), field("lastname", () -> faker.name().lastName()));
-        System.out.println(transformer.generate(fromScratch, limit));
-        // Schema for transformations
-        Schema<Name, String> schemaForTransformations = Schema.of(field("firstName", Name::firstName), field("lastname", Name::lastName));
-        // Here we pass a collection of Name objects and extract first and lastnames from each element
-        System.out.println(transformer.generate(faker.collection(faker::name).maxLen(limit).generate(), schemaForTransformations));
     }
 
 }
