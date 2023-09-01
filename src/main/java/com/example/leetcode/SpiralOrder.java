d8bd82a0724526db0202c0bed4636c0e094f6f89
masterliu <136073894@qq.com>
2023-09-01 18:39:17 +0800

    Revert "add LeetCode array"
    This reverts commit 2343fc24fca138fbfa072af5acb48bb4bbf0d3e4.

--- src/main/java/com/example/leetcode/SpiralOrder.java
+++ src/main/java/com/example/leetcode/SpiralOrder.java
@@ -2,14 +2,10 @@ package com.example.leetcode;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * 剑指 Offer 29. 顺时针打印矩阵
- * 54. 螺旋矩阵
  */
 public class SpiralOrder {
 
@@ -17,60 +13,52 @@ public class SpiralOrder {
     public void test() {
 
         int[][] matrix = {{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}};
-        // [1,2,3,4,5,6,7,8,9,10]
-        System.out.println(spiralOrder(matrix));
-        // [1,2,3,6,9,8,7,4,5]
-        System.out.println(spiralOrder(new int[][] {{1,2,3},{4,5,6},{7,8,9}}));
-        // [1,2,3,4,8,12,11,10,9,5,6,7]
-        System.out.println(spiralOrder(new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
+
+        int[] ans = spiralOrder(matrix);
+
+        System.out.println(Arrays.toString(ans));
     }
 
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public int[] spiralOrder(int[][] matrix) {
 
         int m = matrix.length;
         if (m == 0) {
-            return Collections.emptyList();
+            return new int[] {};
         }
         int n = matrix[0].length;
-        if (n == 0) {
-            return Collections.emptyList();
-        }
-
-        int[][] directions = {{0,1}, {1, 0}, {0,-1}, {-1,0}};
-
-        int size = m * n;
-        List<Integer> ans = new ArrayList<>(size);
-        int flag = 0;
-        int row = 0;
-        int col = 0;
-        int top = 0;
-        int bottom = m;
-        int left = 0;
-        int right = n;
-        ans.add(matrix[0][0]);
-        int index = 1;
-        while (index < size) {
-            int nextRow = row + directions[flag % 4][0];
-            int nextCol = col + directions[flag % 4][1];
-            if (nextRow < top || nextRow == bottom || nextCol < left || nextCol == right) {
-                switch (flag % 4) {
-                    // 左 -> 右
-                    case 0 -> top++;
-                    // 上 -> 下
-                    case 1 -> right--;
-                    // 右 -> 左
-                    case 2 -> bottom--;
-                    // 下 -> 上
-                    default -> left++;
-                }
-                // 转向
-                flag++;
-                continue;
+        // 行边界
+        int row1 = 0;
+        int row2 = m - 1;
+        // 列边界
+        int col1 = 0;
+        int col2 = n - 1;
+        int[] ans = new int[m * n];
+        int index = 0;
+        while (row1 <= row2 && col1 <= col2) {
+            // 左 -> 右
+            for (int j = col1; j <= col2; j++) {
+                ans[index++] = matrix[row1][j];
             }
-            row = nextRow;
-            col = nextCol;
-            ans.add(matrix[row][col]);
-            index++;
+            // 上 -> 下
+            for (int i = row1 + 1; i <= row2; i++) {
+                ans[index++] = matrix[i][col2];
+            }
+            if (row1 != row2) {
+                // 右 -> 左
+                for (int j = col2 - 1; j >= col1; j--) {
+                    ans[index++] = matrix[row2][j];
+                }
+            }
+            if (col1 != col2) {
+                // 下 -> 上
+                for (int i = row2 - 1; i > row1; i--) {
+                    ans[index++] = matrix[i][col1];
+                }
+            }
+            row1++;
+            row2--;
+            col1++;
+            col2--;
         }
 
         return ans;
